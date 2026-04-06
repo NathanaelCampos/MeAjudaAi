c3d9a3fb31257032ff60ccc582a31b70e1a13b55
Nathanael <nathanael_campos@hotmail.com>
2026-04-06 14:19:25 -0300

    Clean backend CI warnings

--- backend/tests/MeAjudaAi.IntegrationTests/Infrastructure/TestAuthenticationHandler.cs
+++ backend/tests/MeAjudaAi.IntegrationTests/Infrastructure/TestAuthenticationHandler.cs
@@ -24,9 +24,8 @@ public class TestAuthenticationHandler : AuthenticationHandler<AuthenticationSch
     public TestAuthenticationHandler(
         IOptionsMonitor<AuthenticationSchemeOptions> options,
         ILoggerFactory logger,
-        UrlEncoder encoder,
-        ISystemClock clock)
-        : base(options, logger, encoder, clock)
+        UrlEncoder encoder)
+        : base(options, logger, encoder)
     {
     }
 
